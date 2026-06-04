66434ba3c8fee500a667e106b374a8427c17225d
afjal4 <afjal.nc@gmail.com>
2026-06-04 01:37:28 +0100

    to add: render

--- simulation.py
+++ simulation.py
@@ -27,23 +27,29 @@ class Simulation(ABC):
             F = np.array([0, 0, -self.rho * 4/3 * np.pi * self.r**3])  # gravity
             for j in range(self.n):
                 if i != j:
-                    F += self.S(self.x[j], self.v[j])(self.x[i])  # stokeslet
+                    F += self.S(j)(i)  # stokeslet interaction
             self.v[i] = F / (6 * np.pi * self.eta * self.r)  # Stokes' law
 
         # Update time
         self.t += self.dt
 
     def run(self, steps):
+        states = []
         for _ in range(steps):
             self.update()
+            states.append(self.x.copy())
+        return states
 
-    def S(self, x_0, F):
-        return lambda x: self.stokeslet(x, x_0, F)
+    def S(self, m):
+        return lambda i: self.stokeslet(self.x[i], self.x[m], self.v[m])
 
-    def stokeslet(self, x, x_0, F):
+    def stokeslet(self, x, x_0, F) -> np.ndarray:
         r = np.linalg.norm(x - x_0)
         c = 1/(8*np.pi*self.eta*r)
         return c * (np.eye(3) + np.outer(x - x_0, x - x_0)/r**2) * F
+    
+    def render(self):
+        pass
 
 
 class SingleParticleSim(Simulation):
